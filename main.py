32365f9f4a0a801c6c7b1bb154374995b58a97ac
Thomas Komlev <ttkoml39>
2020-01-28 11:09:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
-# Add a label with the text "Hello"
-lbl = Label(window, text="Hello")
+# Add a label with the text "Welcome"
+lbl = Label(window, text="Welcome")
 
 # Place the label in the window at 0, 0
 lbl.grid(column=0, row=0)

67a6c2aea0cbcbd918c07cf1a9177596aa7d4ebf
Thomas Komlev <ttkoml39>
2020-01-30 11:16:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 # A starter program for Python with Tkinter
 
-from tkinter import * # import Tkinter library
-window = Tk()         # Create the application window
+from tkinter import * # import Tkinter library        # Create the application window
 
 # Add a label with the text "Welcome", font=("Arial Bold",50))
-lbl = Label(window, text="Welcome")
 
-# Place the label in the window at 0, 0
-lbl.grid(column=0, row=0)
-
-
-window.mainloop()     # Keep the window open
-from tkinter import *
-window = Tk()
+   # Keep the window open
+window = Tk() 
 window.title("Welcome to LikeGeeks app")
-window.mainloop()
 lbl = Label(window, text="Welcome")
 lbl.grid(column=0, row=0)
-from tkinter import*
-window = Tk()
-window.title("welcome to LikeGeeks app")
 lbl = Label(window, text="Welcome")
 lbl.grid(column=0, row=0)
-window.mainloop()
 lbl = Label(window, text="Welcome", font=("Arial Bond",50))
 window.geometry('350x200')
 btn = Button(window, text="Click Me")
 btn.grid(column=1, row=0)
-from tkinter import*
-window = Tk()
-window.title("Welcomr to LikeGeeks app")
-window.geometry('350x200')
 lbl = Label(window, text="welcome")
+lbl.grid(column=0, row=0)
+btn = Button(window, text="Click Me")
+btn.grid(column=1, row=0)
+btn = Button(window, text="Click Me", bg="orange",fg="red")
+def clicked():
+    lbl.configure(text="Button was clicked !!")
+lbl = Label(window, text="Welcome")
+lbl.grid(column=0, row=0)
+btn = Button(window, text="Click Me", command=clicked)
+btn.grid(column=2, row=2)
+txt = Entry(window,width=10)
+txt.grid(column=1, row=0)
+def cicked():
+    lbl.configure(text="Button was clicked !!")
+txt = Entry(window,width=10)
+window.mainloop()
